bfa1ccbd606bd20c25b51ef1516c193a804c550b
Pablo Limo <Pablo.Limo@dealerbuilt.com>
2020-04-10 00:35:48 -0500

    add cool ascii logo

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 'use strict';
 
+const gradient = require('gradient-string');
 const program = require('commander');
 const docstrap = require('./src/docstrap');
 
 program.version(require('./package.json').version);
-program.command('docstrap [dir]')
+program.command('docstrap [dir]');
+
+console.log('\n' + gradient.rainbow('~ D O C S T R A P J S   ♥') + '\n');
 
 docstrap(program);
 
